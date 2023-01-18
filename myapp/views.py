ef6a0112760d6021619c1d2500a60bc04852fed7
Patrick Lin <patrickjylin@gmail.com>
2023-01-18 16:17:51 -0500

    New files, functions, and htmls

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from myapp import support_functions
+from myapp.models import Currency
 
 # Create your views here.
 
@@ -8,3 +12,23 @@ def home(request):
     time = datetime.datetime.now()
     data["time_of_day"] = time
     return render(request, "home.html", context=data)
+
+def maintenance(request):
+    data = dict()
+    try:
+        choice = request.GET['selection']
+        if choice == "currencies":
+            support_functions.add_currencies(support_functions.get_currency_list())
+            c_list = Currency.objects.all()
+            print("Got c_list", len(c_list))
+            data['currencies'] = c_list
+            return HttpResponseRedirect(reverse('currencies'))
+    except:
+        pass
+    return render(request, "maintenance.html", context=data)
+
+def view_currencies(request):
+    data = dict()
+    c_list = Currency.objects.all()
+    data['currencies'] = c_list
+    return render(request, "currencies.html", context=data)
